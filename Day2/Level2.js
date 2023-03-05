6dd93f7e35b2874fbed8a3d216da2784a7ce3be0
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-05 17:40:12 +0300

    1 file modified, 3 added

---
+++ Day2/Level2.js
@@ -0,0 +1,29 @@
+let sentence ='The quote \'There is no exercise better for the heart than reaching down and lifting people up.\' by John Holmes teaches us to help one another.'
+console.log(sentence)
+let sentence2 ='"Love is not patronizing and charity isn\'t about pity, it is about love. Charity and love are the same -- with charity you give love, so don\'t just give money but reach out your hand instead."'
+console.log(sentence2)
+let a = parseInt( '10')
+let b = 10
+
+console.log(typeof(a)==typeof(b))
+let c = '9.8'
+console.log(b==Math.round(c))
+let word1='paython'
+let word2='jargon'
+console.log(word1.includes('on'))
+console.log(word2.includes('on'))
+let sentence3 = 'I hope this course is not full of jargon.'
+console.log(sentence3.includes('jargon'))
+
+console.log(Math.random()*101)// 0 - 100
+ console.log((Math.random()*51) + 50)// 50 - 100
+ console.log(Math.random()*256)// 0 - 255
+ let word='JavaScript'
+ console.log(word[0])//J
+let escape =' '
+
+ 
+ console.log('1 1 1 1 1\n2 1 2 4 8\n3 1 3 9 27\n4 1 4 16 64\n5 1 5 25 125\n')
+  let sentence4='You cannot end a sentence with because because because is a conjunction'
+ 
+  console.log(sentence4.substring(31,55))
