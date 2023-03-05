fc5e31f3f6311a48a99c41a8bd8b2afc90fd7d1b
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-06 02:47:46 +0300

    1 file modified, 3 added

---
+++ Day3/level1.js
@@ -0,0 +1,57 @@
+let firistName = 'abebe'
+let lastName = 'debebe'
+let country = 'ethiopia'
+let age =22
+let isMarried = true
+let year = 2023
+console.log(typeof(firstName))
+console.log(typeof(lastName))
+console.log(typeof(country))
+console.log(typeof(isMarried))
+console.log(typeof(age))
+console.log(typeof(year))
+
+console.log('10' === 10)
+console.log(parseInt('9.8')==10)
+let string='coding is not hard thing to do '
+console.log(string.includes('not'))
+console.log(string.startsWith('c'))
+console.log(string.endsWith(''))
+console.log(string.includes('zz'))
+console.log(string.startsWith('b'))
+console.log(string.endsWith('c'))
+1 ,true
+2 ,true
+3 ,false
+4 , false
+5 ,true
+6 ,true
+7 ,false
+8 ,false
+9 ,false
+10, true
+11 ,false
+let a ='python'
+let b ='jargon'
+console.log(a.length !== b.length) 
+
+1 ,true
+2 ,false
+3 ,true
+4 ,true
+5  ,false
+6 ,true
+7 ,true
+8 ,false
+9 ,true
+10 , true
+11 ,false
+let date = new Date
+console.log(date.getFullYear())
+console.log(date.getMonth())
+console.log(date.getDate())
+console.log(date.getDay())
+console.log(date.getHours())
+console.log(date.getMinutes())
+let date1 = Date.now()
+console.log(date1)
