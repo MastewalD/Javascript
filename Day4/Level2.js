aeba08b9c54e486adeadaf9a6675659e4d58b15f
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-07 16:41:35 +0300

    1 file modified

---
+++ Day4/Level2.js
@@ -0,0 +1,17 @@
+let score = prompt('Enter the score',)
+if( score >= 80)
+{
+  console.log('A')
+} else if(  score >=70)
+{
+  console.log('B')
+} else if( score >= 60)
+{
+  console.log('C')
+} else if( score >= 50)
+{
+  console.log('D')
+} else if( score < 50)
+{
+  console.log('F')
+}
