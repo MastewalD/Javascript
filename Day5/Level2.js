5feca0b84b40e4e4c417fcb0636a366195d271f5
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-08 20:33:18 +0300

    2 files modified

--- Day5/Level2.js
+++ Day5/Level2.js
@@ -12,3 +12,23 @@ console.log(shoppingCart.splice(4,1))
 console.log(shoppingCart)
 console.log(shoppingCart.splice(3,1,"Green tea"))
 console.log(shoppingCart)
+
+const countries = [
+  'Albania',
+  'Bolivia',
+  'Canada',
+  'Denmark',
+  'Ethiopia',
+  'Finland',
+  'Germany',
+  'Hungary',
+  'Ireland',
+  'Japan',
+  'Kenya'
+]
+let check =countries.toString().includes("Ethiopia")
+if(check){
+  console.log("ETHIOPIA")
+}else{
+  countries.push("Ethiopia")
+}
--- index.js
+++ index.js
@@ -4,17 +4,3 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
-const countries = [
-  'Albania',
-  'Bolivia',
-  'Canada',
-  'Denmark',
-  'Ethiopia',
-  'Finland',
-  'Germany',
-  'Hungary',
-  'Ireland',
-  'Japan',
-  'Kenya'
-]
