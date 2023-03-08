cf936ca56d0e3b1d3171303123d3cffb46db33d1
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-08 20:39:07 +0300

    1 file modified

--- Day5/Level2.js
+++ Day5/Level2.js
@@ -48,3 +48,8 @@ if(check){
   webTechs.push("Sass")
   console.log(webTechs)
 }
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+const backEnd = ['Node','Express', 'MongoDB']
+
+let fullStack = frontEnd.concat(backEnd)
+console.log(fullStack)
