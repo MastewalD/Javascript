6d1668b625342087fdc17e0174f5ae76c7baf707
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-08 21:35:32 +0300

    2 files modified

--- index.js
+++ index.js
@@ -4,4 +4,7 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
-
+// for loop
+for (let i = 0; i <= 10; i++) {
+  console.log(i);
+}
