260aa50fe7debfb88e1aeaa8f162323313a2f0f8
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-09 13:58:12 +0300

    1 file modified

---
+++ Day6/Level1.js
@@ -0,0 +1,60 @@
+let n = 4;
+for (let i = 0; i <= n; i++) {
+  console.log(i);
+}
+let symbol = '';
+for (let i = 0; i <= 6; i++) {
+  symbol = symbol + '#';
+  console.log(symbol);
+}
+let sqr = 0;
+for (let i = 0; i <= 10; i++) {
+  sqr = i * i;
+  console.log(`${i} x ${i} = ${sqr}`);
+}
+let num = 0;
+let sqr = 0;
+let cub = 0;
+for (let i = 0; i <= 10; i++) {
+  num = i;
+  sqr = i * i;
+  cub = i * i * i;
+  console.log(`${num}  ${sqr}  ${cub}`);
+}
+
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 !== 0) {
+    console.log(i);
+  }
+}
+let sum = 0;
+for (let i = 0; i <= 100; i++) {
+  sum += i;
+}
+console.log(`The sum of all numbers from 0 to 100 is ${sum}`);
+let sum = 0;
+let even = 0;
+let odd = 0;
+for (let i = 0; i <= 100; i++) {
+  if (i % 2 == 0) {
+    even = even + i;
+  } else {
+    odd = odd + i;
+  }
+}
+console.log(
+  `The sum of all evens for o to 100 is${even}. And the sum of all odds from o to 100 is ${odd}.`
+);
+
+let chars = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXTZabcdefghiklmnopqrstuvwxyz';
+let randomStr = '';
+for (let i = 0; i <= 5; i++) {
+  let ranodm = Math.floor(Math.random() * chars.length);
+  randomStr += chars[ranodm];
+}
+console.log(randomStr);
