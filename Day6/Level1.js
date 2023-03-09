4a952154207a585d98564ec0c4a19049eef93a67
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-09 09:34:46 +0300

    2 files modified

--- Day6/Level1.js
+++ Day6/Level1.js
@@ -12,8 +12,7 @@ for( let i=0;i<=10;i++){
   do{
     console.log(i)
   }while(i<=10)
-  let i=0
-do{
-  console.log(i)
-  i++
-}while(i<10)
+  
+  for(let i=10; i>=0; i--){
+    console.log(i)
+  }
