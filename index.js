dce6219a5e63a66667138ea5a0c7862ae116bd75
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-11 19:55:58 +0300

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>one</h1>`;
+let dog = {};
+console.log(dog);
+dog.name = "jasper";
+dog.legs = 4;
+dog.color = "brown"
+dog.age = 4;
+dog.bark = function () {
+    console.log("woof woof woof")
+}
+console.log(Object.values(dog));
+dog.breed = "german shepherd";
+dog.getDogInfo = function () {
+    console.log(Object.values(this));
+}
