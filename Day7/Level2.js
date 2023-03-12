de5b34422dd8cdd64cb9d69461c16d0d972edc5d
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-12 16:10:20 +0300

    2 files modified

--- Day7/Level2.js
+++ Day7/Level2.js
@@ -1,21 +1,61 @@
-function printArray (array){
-  return array
+function printArray(array) {
+  return array;
+}
+console.log(printArray([1, 2, 3, 4, 5]));
+
+function showDateTime() {
+  let date = new Date();
+  let year = date.getFullYear();
+  let month = date.getMonth();
+  let day = date.getDate();
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+  return `${day}/${month}/${year} ${hour}:${minute}`;
+}
+console.log(showDateTime());
+
+function swapValue(a, b) {
+  return `${b} ${a}`;
+}
+console.log(swapValue(5, 3));
+
+const arr = [];
+function reversArray(array) {
+  for (let i = array.length; i > 0; i--) {
+    arr.push(i);
   }
-  console.log(printArray([1,2,3,4,5]))
-  
-  
-  function showDateTime (){
-    let date = new Date()
-    let year=date.getFullYear()
-    let month=date.getMonth()
-    let day=date.getDate()
-    let hour=date.getHours()
-    let minute=date.getMinutes()
-    return `${day}/${month}/${year} ${hour}:${minute}`
+  console.log(arr);
+}
+console.log(reversArray([1, 2, 3, 4, 9]));
+
+
+function capitalizeArray(array) {
+  let cap = array.toString().toUpperCase();
+  return cap;
+}
+console.log(capitalizeArray(['maste', 'kid']));
+let sum = 0;
+function additem(num1, num2) {
+  sum = num1 + num2;
+
+  let array = [];
+  array.push(sum);
+  return array;
+}
+console.log(additem(2, 3));
+const array1 = [1, 2, 3, 4, 5];
+function removeitem(index) {
+  array1.splice(index, 1);
+  return array1;
+}
+console.log(removeitem(3));
+
+function sumOfNumbers(numStart, numEnd) {
+  let sum = 0;
+
+  for (let i = numStart; i <= numEnd; i++) {
+    sum += i;
+    console.log(sum);
   }
-  console.log(showDateTime())
-  
-  function swapValue (a,b){
-  return `${b} ${a}`
-  }
-  console.log(swapValue(5,3))
+}
+sumOfNumbers(1, 4);
--- index.js
+++ index.js
@@ -4,19 +4,3 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>one</h1>`;
-
-
-
-function reversArray (array){
-for(let i=array.length;i>=0;i--){
-
-const arr=[]
-arr.push(i)
-
-
-
-}
-}
-console.log(reversArray(array))
-
-console.log('maste')
