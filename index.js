46741f5b4a2a0904abd801b097f3adacaf76193e
mastewaldegu123 <126682436+mastewaldegu123@users.noreply.github.com>
2023-03-12 22:07:42 +0300

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,7 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>one</h1>`;
+const names = ['Asabeneh', 'Mathias', 'Elias', 'Brook']
+names.forEach(num=>console.log(num.toUpperCase()))
+const number=names.map(n=>n.toUpperCase())
+console.log(number)
